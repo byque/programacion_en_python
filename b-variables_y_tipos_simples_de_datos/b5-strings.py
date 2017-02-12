1e01ecde73b1631ec52f1b9b255b9f554d5e68bb
byque <byque2013@gmail.com>
2017-02-12 15:23:36 +0100

    Añadí strings.py

---
+++ b-variables_y_tipos_simples_de_datos/b5-strings.py
@@ -0,0 +1,14 @@
+# Las variables de tipo string son cadenas de caracteres.
+mensaje1 = "Esto es un string"
+mensaje2 = 'Esto también es un string'
+
+# Se puede usar comillas o apóstrofes para definir strings
+# Su uso depende del programador y también si se va a usar
+# comillas o apóstrofes dentro del mensaje.
+mensaje3 = 'Esto es un string con "comillas"'
+mensaje4 = "Esto es un string con 'apóstrofes'"
+
+print(mensaje1)
+print(mensaje2)
+print(mensaje3)
+print(mensaje4)
