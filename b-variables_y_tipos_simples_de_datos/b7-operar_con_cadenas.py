6d10c136d8970859d9e6af969513d004b593cebb
byque <byque2013@gmail.com>
2017-02-12 16:01:23 +0100

    Añadí operar_con_cadenas.py

---
+++ b-variables_y_tipos_simples_de_datos/b7-operar_con_cadenas.py
@@ -0,0 +1,14 @@
+nombre = "simón"
+apellido = "bolívar"
+
+# Concatenar cadenas
+nombre_completo = nombre + " " + apellido
+
+print(nombre_completo.title())
+
+# Concatenar al imprimir
+print("Hola " + nombre_completo.title())
+
+# Guardar como variable antes de imprimir
+mensaje = "Hola " + nombre_completo.title() + ", es un placer."
+print(mensaje)
