5291499df058cab690bbdbeab67588eac691f41c
byque <byque2013@gmail.com>
2017-02-12 15:05:53 +0100

    Añadí hola mundo con error

---
+++ b-variables_y_tipos_simples_de_datos/b3-hola_mundo_con_error.py
@@ -0,0 +1,10 @@
+mensaje = "¡Hola mundo Python!"
+print(mesaje)
+
+# Produce el siguiente error:
+# Traceback (most recent call last):
+#  File "b3-hola_mundo_con_eror.py", line 2, in <module>
+#     print(mesaje)
+# NameError: name 'mesaje' is not defined
+
+# El error se encuentra en la línea 2 y es que 'mesaje' no está definido.
