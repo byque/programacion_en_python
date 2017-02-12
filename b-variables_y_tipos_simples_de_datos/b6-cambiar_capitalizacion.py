985c0b66e50481a67dcdff38dd098850617debf3
byque <byque2013@gmail.com>
2017-02-12 15:46:22 +0100

    Añadí cambiar_capitalización.py

---
+++ b-variables_y_tipos_simples_de_datos/b6-cambiar_capitalizacion.py
@@ -0,0 +1,11 @@
+# Primer uso de métodos ver 'nombre.title()'
+nombre = "simón Bolívar"
+
+# Imprime la primera letra de cada palabra en mayúsculas.
+print(nombre.title())
+
+# Imprime todo en minúsculas.
+print(nombre.lower())
+
+# Imprime todo en mayúsculas.
+print(nombre.upper())
