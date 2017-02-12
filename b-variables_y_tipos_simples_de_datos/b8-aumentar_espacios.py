4a243cee5585723ecbd7890af68dfa610da9f379
byque <byque2013@gmail.com>
2017-02-12 16:21:11 +0100

    Añadí aumentar_espacios.py

---
+++ b-variables_y_tipos_simples_de_datos/b8-aumentar_espacios.py
@@ -0,0 +1,8 @@
+# Añadir un espacio de tabulador al texto
+print("\tEspacio de tabulador")
+
+# Añadir una línea nueva
+print("Esta es una\nLínea nueva")
+
+# Añadir un tabulador y una línea nueva
+print("Tabulador + línea nueva:\n\tSe ve así.")
