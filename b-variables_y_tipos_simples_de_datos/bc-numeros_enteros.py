e072c1eef2765665d18be1a60199b61fbd943733
byque <byque2013@gmail.com>
2017-02-13 14:40:32 +0100

    Añadí numeros_enteros.py

---
+++ b-variables_y_tipos_simples_de_datos/bc-numeros_enteros.py
@@ -0,0 +1,21 @@
+# Enteros
+
+# Suma
+print(1+3)
+
+# Los espacios no afectan la operación y mejoran la lectura
+resta = 3 - 1
+print(resta)
+
+# Multiplicación
+print(4 * 3)
+
+# División
+print(6 / 2)
+
+# Exponenciales
+print(2 ** 2)
+
+# Orden de operaciones
+print(3 + 3*4)
+print((3+3) * 4)
