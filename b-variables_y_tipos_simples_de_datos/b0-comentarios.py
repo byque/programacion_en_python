def9c37a0fd549d4979eb0b0a0fc4b33881bdbdc
byque <byque2013@gmail.com>
2017-02-16 22:55:32 +0100

    Añadí comentarios.py

---
+++ b-variables_y_tipos_simples_de_datos/b0-comentarios.py
@@ -0,0 +1,7 @@
+# Los comentarios empiezan con un '#' y sirven para añadir notas al programa
+# para describir la solución implementada en el código.
+
+# Todo lo que está después de un '#' es ignorado por el intérprete de Python.
+
+# La siguiente línea imprime ¡Hola! como salida en el monitor
+print("¡Hola!")
