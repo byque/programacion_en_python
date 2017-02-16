5e8b8c4360c6e094ca544b582592ad752a24832b
byque <byque2013@gmail.com>
2017-02-17 00:00:32 +0100

    Añadí bicicletas.py

---
+++ c-listas/c1-bicicletas.py
@@ -0,0 +1,7 @@
+# Es una buena idea poner en plural el nombre de una lista, ya que ésta
+# contiene varios elementos.
+
+# En Python se usa [] para indicar una lista y sus elementos se separan
+# por comas
+bicicletas = ['trek', 'santa cruz', 'specialized', 'orbea']
+print(bicicletas)
